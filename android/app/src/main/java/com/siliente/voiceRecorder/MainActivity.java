0e5b994750336772a9307b035da7239240b43db6
Mehmet Uysal <mehmetuysal2000@yandex.com>
2022-11-19 16:17:16 +0300

    1.0.2
    backup for "vue add @vue/pwa"

--- android/app/src/main/java/com/siliente/voiceRecorder/MainActivity.java
+++ android/app/src/main/java/com/siliente/voiceRecorder/MainActivity.java
@@ -3,7 +3,7 @@ package com.siliente.voiceRecorder;
 import com.getcapacitor.BridgeActivity;
 import android.os.Bundle;
 
-public class MainActivity extends BridgeActivity {
+public class MainActivity extends BridgeActivity, Service {
   @Override
   public void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,10 @@
 //import { adMobBanner, adMobInitialize } from './services/capacitor/addmob';
 export default {
   mounted() {
+    if ("serviceWorker" in navigator) {
+      navigator.serviceWorker.register("@/services/web/serviceWorker.js");
+    }
     //adMobInitialize().then(() => adMobBanner())
   },
-}
+};
 </script>
